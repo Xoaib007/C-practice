89d8cc854a52d2cfa9de1c8b580ff5f92cad5562
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-03-05 23:33:38 +0100

    Array

---
+++ Week 2/bulbs.c
@@ -0,0 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    string word = get_string("Message: ");
+
+    int count[]= {};
+    for(int i= 0; i < strlen(word); i++)
+    {
+        int decimal = word[i];
+
+        int binary[BITS_IN_BYTE];
+        int q= decimal;
+        int r;
+
+        for(int j = 0; j < BITS_IN_BYTE ; j++)
+        {
+            r = q % 2;
+            binary[j]= r;
+            q /= 2;
+        };
+
+        for(int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+
+        printf("\n");
+    }
+
+}
+
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
