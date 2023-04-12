113a33e9966a7a9528538ace02986ac891b8f55c
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-04-12 20:45:20 +0200

    Update pointers.c

--- Week 4/pointers.c
+++ Week 4/pointers.c
@@ -1,5 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(void){
+  int a = 28;
+  int b = 50;
+  int *c = &a;
+  
+  *c = 14;
+  c = &b;
+  *c = 25;
+  
+  // print result
+  printf("a has the value of %i, located at %p\n", a. &a);
+  printf("b has the value of %i, located at %p\n", b. &b);
+  printf("c has the value of %i, located at %p\n", c. &c);
+}
   
